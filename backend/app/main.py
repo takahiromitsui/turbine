ef56047f7e330960e22b4f378d6f760a02d16298
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-06-03 15:26:01 +0200

    refactor: move database

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,5 @@
-import os
-from dotenv import load_dotenv
-from pymongo import MongoClient
-
-load_dotenv()
-
-password = os.getenv("MONGO_PWD")
-
-connection_string = f"mongodb+srv://taka:{password}@cluster0.qtybgjl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-client = MongoClient(connection_string)
+# local
+from app.db.database import client
 
 def insert_data(data):
     db = client.get_database('test')
