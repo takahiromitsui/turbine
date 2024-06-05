735fe1d04bbab342d7e64dbe9b54cb85b9c72b7a
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-06-05 13:40:39 +0200

    feat: sort by time

--- backend/app/services/turbine.py
+++ backend/app/services/turbine.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+from pymongo import ASCENDING
+
+# local imports
 from app.db.database import DB_TASK_TWO
 from app.models.database import Turbine
-from datetime import datetime
 
 db = DB_TASK_TWO
 
@@ -16,7 +19,7 @@ def get_turbine_data(
                 "time": {"$gte": start_date, "$lte": end_date},
             },
             {"_id": 0},
-        )
+        ).sort("time", ASCENDING)
 
         return [Turbine(**turbine) for turbine in data]
 
