cca8d3a2b42f3c6e883a61e55e771ab8d04937a8
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-06-05 16:03:38 +0200

    feat: create Datetime picker

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+
+import { DateTimePicker } from "@/components/ui/date-time-picker/date-time-picker";
 import LineGraph from "@/components/line";
 
 export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
-			<h1 className='text-6xl'>Welcome to my website</h1>
+			<DateTimePicker granularity={"minute"} />
       <LineGraph />
 		</main>
 	);
