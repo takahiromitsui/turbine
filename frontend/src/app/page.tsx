3f525294fa47ebacd4815c6d12cb89603d3383b6
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-06-06 10:30:27 +0200

    feat: create default layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,40 @@
 'use client';
 
-// import LineGraph from "@/components/line";
-import TurbineDateRangePicker from '@/components/turbine-date-range-picker';
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select';
 
 export default function Home() {
 	return (
-		<main className=''>
-			<TurbineDateRangePicker />
-			{/* <LineGraph /> */}
-		</main>
+		<div className='flex h-screen'>
+			<div className='w-1/4 bg-primary pt-4 pl-4 pr-4'>
+				{/*Sidebar content*/}
+				<h2 className='text-white text-2xl font-bold pb-4'>Turbine</h2>
+				<ul>
+					<Select>
+						<SelectTrigger>
+							<SelectValue placeholder='Select a Turbine' />
+						</SelectTrigger>
+						<SelectContent>
+							<SelectGroup>
+								<SelectLabel>Turbine ID</SelectLabel>
+								<SelectItem value='1'>1</SelectItem>
+								<SelectItem value='2'>2</SelectItem>
+							</SelectGroup>
+						</SelectContent>
+					</Select>
+				</ul>
+			</div>
+			<div className='flex-1 bg-background pt-4 pl-4 pr-4'>
+				{/*Main content*/}
+				<h1 className='text-4xl font-bold mb-4'>Main Content: Graph Here</h1>
+			</div>
+		</div>
 	);
 }
