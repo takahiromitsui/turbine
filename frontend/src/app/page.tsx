5ad87c92c3cf7ff1379badee31552cd56079b073
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-06-06 11:05:37 +0200

    feat: add start date and end date

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { DateTimePicker } from '@/components/ui/date-time-picker/date-time-picker';
 import {
 	Select,
 	SelectContent,
@@ -25,20 +26,31 @@ export default function Home() {
 		<div className='flex h-screen'>
 			<div className='w-1/4 bg-primary pt-4 pl-4 pr-4'>
 				{/*Sidebar content*/}
-				<h2 className='text-white text-2xl font-bold pb-4'>Turbine</h2>
+				<h2 className='text-input text-2xl font-bold pb-8'>Turbine</h2>
 				<ul>
-					<Select onValueChange={handleTurbineID}>
-						<SelectTrigger>
-							<SelectValue placeholder='Select a Turbine' />
-						</SelectTrigger>
-						<SelectContent>
-							<SelectGroup>
-								<SelectLabel>Turbine ID</SelectLabel>
-								<SelectItem value='1'>1</SelectItem>
-								<SelectItem value='2'>2</SelectItem>
-							</SelectGroup>
-						</SelectContent>
-					</Select>
+					<li className='pb-24'>
+						<span className='text-input'>Turbine ID</span>
+						<Select onValueChange={handleTurbineID}>
+							<SelectTrigger>
+								<SelectValue placeholder='Select a Turbine' />
+							</SelectTrigger>
+							<SelectContent>
+								<SelectGroup>
+									<SelectLabel>Turbine ID</SelectLabel>
+									<SelectItem value='1'>1</SelectItem>
+									<SelectItem value='2'>2</SelectItem>
+								</SelectGroup>
+							</SelectContent>
+						</Select>
+					</li>
+					<li className='pb-4'>
+						<span className='text-input'>Start Date</span>
+						<DateTimePicker granularity={'minute'} />
+					</li>
+					<li>
+						<span className='text-input'>End Date</span>
+						<DateTimePicker granularity={'minute'} />
+					</li>
 				</ul>
 			</div>
 			<div className='flex-1 bg-background pt-4 pl-4 pr-4'>
