8b3a8593c40a951a3041ba5f8144d64c31a687c1
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-06-07 17:29:04 +0200

    feat: add initialize_db command

---
+++ backend/app/db/initialize_db.py
@@ -0,0 +1,44 @@
+from app.db.database import DB_TASK_ONE, DB_TASK_TWO
+from app.services.data_loader import DataLoaderFromAPI, DataLoaderFromCSV
+
+
+def initialize_db():
+    # Initialize the database
+    print("Process Task1 data")
+    exist_task_one_data = False
+    if DB_TASK_ONE.posts.count_documents({}) > 0:
+        exist_task_one_data = True
+
+    if not exist_task_one_data:
+        data_loader = DataLoaderFromAPI(db=DB_TASK_ONE)
+        data_loader.load_data_from_api()
+        print("Task1 data loaded")
+    else:
+        print("Task1 data already exists")
+
+    print("Process Task2 data")
+    exist_task_two_data = False
+    if DB_TASK_TWO.turbine.count_documents({}) > 0:
+        exist_task_two_data = True
+
+    if not exist_task_two_data:
+        DataLoaderFromCSV(
+            db=DB_TASK_TWO,
+            url="https://nextcloud.turbit.com/s/GTbSwKkMnFrKC7A/download/Turbine1.csv",
+            turbine_id=1,
+        ).load_data_from_csv()
+        print("Task2 data loaded from Turbine1.csv")
+        DataLoaderFromCSV(
+            db=DB_TASK_TWO,
+            url="https://nextcloud.turbit.com/s/G3bwdkrXx6Kmxs3/download/Turbine2.csv",
+            turbine_id=2,
+        ).load_data_from_csv()
+        print("Task2 data loaded from Turbine2.csv")
+    else:
+        print("Task2 data already exists")
+
+    print("Database initialized")
+
+
+if __name__ == "__main__":
+    initialize_db()
